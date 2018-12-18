2c1f8d52f3076b592215295e1c1bab84e8af97b5
Leslio <reuangelomckeown@gmail.com>
2018-12-17 23:50:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,95 +3,55 @@ Treehouse Techdegree:
 FSJS project 1 - A Random Quote Generator
 Name: Leslio McKeown
 Date Started: 12/12/2018
-Date Finished 12//2018
+Date Finished 12/17/2018
 ******************************************/
-
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-var message = '';
-var red;
-var green;
-var blue;
-var viewed_quotes =[];
-
-function getRandomQuote(){
 var quotes = [
-{quote: "The hole in one's heart hets filled by others around you. - Kakashi Hatake",
-author: "Naruto shippuden"
+{quote: "The hole in one's heart hets filled by others around you.",
+source: " Kakashi Hatake,Naruto shippuden"
 },
 {
-  quote: "We can't waste time worrying about the what-if's. - Ichigo Kurosaki",
-  author: "Bleach"
+  quote: "We can't waste time worrying about the what-if's.",
+  source: " Ichigo Kurosaki, Bleach"
 },
 {
-  quote: "Even your gravest mistakes shouldn't stop you from moving forward. - Edward Elric",
-  author: "Fullmetal Alchemist"
+  quote: "Even your gravest mistakes shouldn't stop you from moving forward.",
+  source: " Edward Elric, Fullmetal Alchemist"
 },
 {
   quote: "It's okay not to be okay as long as you are not giving up.",
-  author: " - Karen Salmansohn "
+  source: " Karen Salmansohn"
 
 },
 {
   quote: "The truth will set you free, but first it will make you miserable.",
-  author: " - James A. Garfield"
+  source: " James A. Garfield"
     }
   ];
+// this function is to generate a random number and then pick a quote that is
+// equel to the num it has choosen
+  function getRandomQuote() {
+  var pickQuote = Math.floor(Math.random()* quotes.length);
+  return (quotes[pickQuote]);
+
 }
 
-<body>
-<h1> Simple Quote Generator</h1>
-<div id_"quote_display"> </div>
-
-<button onclick="new_quote()" > new quote </button> /* this will have a button show up
-with new quote on it, click and a new quote will appear */
-
-<script src="javascript.js"></script>
-</body>
-
-// this code is to add - in the quote referring to whom said it.
-function randomQuote (quotes){
-
-  for(var i = 0; i < quotes.length; i +=1){
-    var pickQuote = Math.floor(MAth.random() * quotes.length);
-    pickQuote += quotes[i].quote + ' - ' + quotes[i].author;
+// this is setting the quote in motion by seperating the
+// quote and source in different fonts and seize
+//after that it prints it out
+  function printQuote() {
+  var print = getRandomQuote();
+  console.log(print);
+  var data = '<p class="quote">';
+  data += print.quote;
+  data += '</p>';
+  data += '<p class="source">';
+  data += print.source;
+  data += '</p>';
+  document.getElementById('quote-box').innerHTML = data; // this is how the HTML
+  // works with the JS functions and command.
+  console.log(data);
   }
-  return pickQuote
-}
 
-console.log(randomQuote(quotes));
-
-function printQuote(quote){
-var output_div = document.getElementById('quote-box');
-output_div.innerHTML = quotes;
-}
-
-function getRandomQuote(){
-var getRandomQuote = math.floor(Math.random() * quotes.length);
-var spliced_quote = quotes.splice(getRandomQuote, 1)[0];
-viewed_quotes.push(spliced_quote);
-if (quotes.length === 0){
-  quotes = viewed_quotes;
-  viewed_quotes = [];
-}
-  return spliced_quote
-}
-// this will make the background color change for each quote that was changed
-function random_color_genarator(){
-var random_color;
-red = Math.floor(Math.random() * 256);
-green = Math.floor(Math.random() * 256);
-blue = Math.floor(Math.random() * 256);
-random_color = 'rgb(' + red + "," + green ',' + blue + ')';
-return random_color
-}
-
-function printQuote(){
-  var quotes = getRandomQuote();
-	print(message);
-  //Generate random color
-  randomColorGenerator();
-  //Update background with new random color
-   document.getElementById('bgcolor').style.backgroundColor = randomColorGenerator();
-}
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// when button is pressed/clicked a new quotes will appear
+// depending on the random number
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
